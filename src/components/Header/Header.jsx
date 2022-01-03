db67f8b03acc0e83fe642f93168373b101f57783
abeshi03 <k_913411@yahoo.co.jp>
2022-01-03 12:07:12 +0900

    Header: 作成完了

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,13 @@ import styles from "./Header.module.scss";
 export const Header = () => {
   return (
     <header>
+      <div className={styles.headerInner}>
+        <div className={styles.logo}>Profile</div>
+        <div className={styles.left} role="navigation">
+          <a className={styles.link} hreh="#" role="link">About</a>
+          <a className={styles.link} hreh="#" role="link">Bicycle</a>
+        </div>
+      </div>
     </header>
   )
 }
